1d868fb0b88f9d3f9633a1439b936db2eb9622d2
Daniel Betancur <125151874+dbetancurdev@users.noreply.github.com>
2026-07-09 16:12:32 -0500

    Update authentication.component.spec.js

--- tests/authentication.component.spec.js
+++ tests/authentication.component.spec.js
@@ -1,59 +1,67 @@
-const assert = require('assert)')
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of, throwError } from 'rxjs';
+import { PasswordComponent } from './password.component';
+import { PasswordService } from './password.service';
 
-describe('Component Tests', () => {
-  describe('PasswordComponent', () => {
+describe('PasswordComponent', () => {
+  let comp: PasswordComponent;
+  let fixture: ComponentFixture<PasswordComponent>;
+  let service: PasswordService;
 
-    let comp
-    let service
-
-    test('should show error if passwords do not match', () => {
-      // GIVEN
-      comp.password = 'password1';
-      comp.confirmPassword = 'password2';
-      // WHEN
-      comp.changePassword();
-      // THEN
-      assert(comp.doNotMatch).toBe('ERROR');
-      assert(comp.error).toBeNull();
-      assert(comp.success).toBeNull();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PasswordComponent],
+      providers: [PasswordService],
     });
+    fixture = TestBed.createComponent(PasswordComponent);
+    comp = fixture.componentInstance;
+    service = TestBed.inject(PasswordService);
+  });
 
-    test('should call Auth.changePassword when passwords match', () => {
-      // GIVEN
-      // deepcode ignore NoHardcodedPasswords/test: <please specify a reason of ignoring this>
-      comp.password = comp.confirmPassword = 'myPassword';
+  it('should show error if passwords do not match', () => {
+    // GIVEN
+    comp.password = 'password1';
+    comp.confirmPassword = 'password2';
+    // WHEN
+    comp.changePassword();
+    // THEN
+    expect(comp.doNotMatch).toBe('ERROR');
+    expect(comp.error).toBeNull();
+    expect(comp.success).toBeNull();
+  });
 
-      // WHEN
-      comp.changePassword();
+  it('should call the service when passwords match', () => {
+    // GIVEN
+    // deepcode ignore NoHardcodedPasswords/test: dummy value used only in unit test
+    comp.password = comp.confirmPassword = 'myPassword';
+    const saveSpy = jest.spyOn(service, 'save').mockReturnValue(of({}));
+    // WHEN
+    comp.changePassword();
+    // THEN
+    expect(saveSpy).toHaveBeenCalledWith('myPassword');
+  });
 
-      // THEN
-      assert(service.save).toHaveBeenCalledWith('myPassword');
-    });
+  it('should set success to OK on success', () => {
+    // GIVEN
+    comp.password = comp.confirmPassword = 'myPassword';
+    jest.spyOn(service, 'save').mockReturnValue(of({}));
+    // WHEN
+    comp.changePassword();
+    // THEN
+    expect(comp.doNotMatch).toBeNull();
+    expect(comp.error).toBeNull();
+    expect(comp.success).toBe('OK');
+  });
 
-    test('should set success to OK upon success', function() {
-      // GIVEN
-      comp.password = comp.confirmPassword = 'myPassword';
-
-      // WHEN
-      comp.changePassword();
-
-      // THEN
-      expect(comp.doNotMatch).toBeNull();
-      expect(comp.error).toBeNull();
-      expect(comp.success).toBe('OK');
-    });
-
-    test('should notify of error if change password fails', function() {
-      // GIVEN
-      comp.password = comp.confirmPassword = 'myPassword';
-
-      // WHEN
-      comp.changePassword();
-
-      // THEN
-      assert(comp.doNotMatch).toBeNull();
-      assert(comp.success).toBeNull();
-      assert(comp.error).toBe('ERROR');
-    });
+  it('should notify of error if change password fails', () => {
+    // GIVEN
+    comp.password = comp.confirmPassword = 'myPassword';
+    jest.spyOn(service, 'save').mockReturnValue(throwError(() => new Error()));
+    // WHEN
+    comp.changePassword();
+    // THEN
+    expect(comp.doNotMatch).toBeNull();
+    expect(comp.success).toBeNull();
+    expect(comp.error).toBe('ERROR');
   });
 });
